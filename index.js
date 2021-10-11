9c05e93b79ff3522a165da33342466c251a8a7ee
Adriano Lamas <lamas.free@gmail.com>
2021-10-11 17:20:03 -0300

    Cadastro

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 let message = "";
-let titulo = "BlueFlix";
+const titulo = "BlueFlix";
+let catalogo = [
+  {
+    titulo: "teste",
+    imagem:
+      "https://occ-0-1313-1123.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABSAvZbGqC8L4WRNCcNdjXPb9wiFCtqFzrtmKd2Molz-LXOdb4Gl42KvH3bfENEas7_vdXApaFS5uQWuLgzxoiKtxkgd9.webp?r=ae9",
+    sinopse: "teste",
+    ano: 2021,
+    duracao: 100,
+    classificacao: "Livre",
+    categoria: "Comédia",
+    elenco_principal: "teste,teste2,teste3",
+    diretor: "teste",
+  },
+];
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -11,7 +25,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
-  res.render("index",{titulo: titulo});
+  res.render("index", { titulo: titulo, catalogo: catalogo, message });
 });
 
 app.get("/cadastro", (req, res) => {
@@ -19,14 +33,14 @@ app.get("/cadastro", (req, res) => {
 });
 
 app.post("/novo", (req, res) => {
-  message = "Mensagem aqui";
+  message = "Novo filme cadastrado!";
   setTimeout(() => {
     message = "";
   }, 5000);
 
-  //   const pokemon = req.body;
-  //   pokedex.push(pokemon);
-  res.redirect("/");
+  const filme = req.body;
+  catalogo.push(filme);
+  res.redirect("/", { message });
 });
 
 // app.get("/detalhes/:id", (req, res) => {
