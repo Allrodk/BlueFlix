9061a10b5093d50a14bd3b02206130b8c29f439f
Adriano Lamas <lamas.free@gmail.com>
2021-10-14 23:03:13 -0300

    Up

--- controllers/rotaTrailer.js
+++ controllers/rotaTrailer.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const Trailer = require("../models/listaTrailer");
 
+const Trailer = require("../models/listaTrailer");
 const message = "";
 
 // Lista todos os itens do DB
@@ -18,36 +18,54 @@ router.get("/", async (req, res) => {
   res.render("../views/index", { catalogo: trailer, message });
 });
 
-// router.get("/criarProduto", async (req, res) => {
-//   res.render("../views/criarProduto");
-// });
+router.get("/cadastro", async (req, res) => {
+  res.render("../views/cadastro");
+});
 
-// router.post("/novo", async (req, res) => {
-//   const { nome, peso, valor } = req.body;
-//   await Produto.create({
-//     nome: nome,
-//     peso: peso,
-//     valor: valor,
-//   });
-//   res.redirect("/produto");
-// });
+router.post("/novo", async (req, res) => {
+  const {
+    titulo,
+    sinopse,
+    ano,
+    duracao,
+    classificacao,
+    categoria,
+    atores,
+    imagembg,
+    thumb,
+    video,
+  } = req.body;
+  await Produto.create({
+    titulo: titulo,
+    sinopse: sinopse,
+    ano: ano,
+    duracao: duracao,
+    classificacao: classificacao,
+    categoria: categoria,
+    atores: atores,
+    imagembg: imagembg,
+    thumb: thumb,
+    video: video,
+  });
+  res.redirect("/index");
+});
 
-// router.get("/editarProduto/:id", async (req, res) => {
-//   const produto = await Produto.findByPk(req.params.id);
-//   res.render("../views/editarProduto", { produto: produto });
-// });
+router.get("/editar/:id", async (req, res) => {
+  const trailer = await Trailer.findByPk(req.params.id);
+  res.render("../views/editar", { trailer: trailer });
+});
 
-// router.post("/editarProduto/:id", async (req, res) => {
-//   const produto = await Produto.findByPk(req.params.id);
-//   const { nome, peso, valor } = req.body;
+router.post("/editarProduto/:id", async (req, res) => {
+  const produto = await Produto.findByPk(req.params.id);
+  const { nome, peso, valor } = req.body;
 
-//   produto.nome = nome;
-//   produto.peso = peso;
-//   produto.valor = valor;
+  produto.nome = nome;
+  produto.peso = peso;
+  produto.valor = valor;
 
-//   await produto.save();
-//   res.redirect("/produto");
-// });
+  await produto.save();
+  res.redirect("/produto");
+});
 
 // router.get("/deletarProduto/:id", async (req, res) => {
 //   const produto = await Produto.findByPk(req.params.id);
