ecb41826ab19314142a01a2cec8348173a2373c8
Adriano Lamas <lamas.free@gmail.com>
2021-10-17 02:18:56 -0300

    Up Select

--- routes/trailer.route.js
+++ routes/trailer.route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const controller = require("../controllers/trailer.controller");
 
 //Rotas
-router.get("/", controller.home); 
+router.get("/", controller.home);
 router.get("/cadastro", controller.cadastro);
 router.post("/novo", controller.novo);
 router.get("/detalhes/:id", controller.detalhes);
@@ -12,5 +12,4 @@ router.get("/nav", controller.nav);
 router.get("/editar/:id", controller.getEditar);
 router.post("/editar/:id", controller.postEditar);
 router.get("/deletarTrailer/:id", controller.deletar);
-
 module.exports = router;
--- views/public/js/script.js
+++ views/public/js/script.js
@@ -1,62 +1,27 @@
 const closeMessage = document.querySelector("#close");
 const message = document.querySelector("#message");
 
+const classe = document.getElementById("inputClassificacao").value;
+const s = document.getElementById("classificacao");
+const options = ["Livre", "10", "12", "14", "16", "18"];
 
-// Select via DOM
-// const languagesSelect = document.getElementById("listaClassificacao");
-// const languagesList = ["Livre", "10", "12", "14", "16", "18"];
-
-// languagesList.forEach((language) => {
-//   option = new Option(language, language.toLowerCase());
-//   languagesSelect.options[languagesSelect.options.length] = option;
-// });
-
-var s = document.getElementById('listaClassificacao');
-var options = ["Livre", "10", "12", "14", "16", "18"];
-
-options.forEach(function(elemento, chave) {
-  if (elemento == 'Livre') {
-    s.options[s.options.length] = new Option(elemento, elemento, false, true);
+options.forEach((elemento, chave) => {
+  if (elemento !== classe) {
+    s.options[s.options.length] = new Option(elemento, elemento, false, false);
   }
-  else if (elemento == '10') {
-    s.options[s.options.length] = new Option(elemento, elemento, false, true);
-  }
-  else if (elemento == '12') {
-    s.options[s.options.length] = new Option(elemento, elemento, false, true);
-  }
-  else if (elemento == '14') {
-    s.options[s.options.length] = new Option(elemento, elemento, false, true);
-  }
-  else if (elemento == '16') {
-    s.options[s.options.length] = new Option(elemento, elemento, false, true);
-  }
-  else if (elemento == '18') {
+  else {
     s.options[s.options.length] = new Option(elemento, elemento, false, true);
   }
 });
 
-/* <select name="classificacao" id="classificacao">                    
-<option value="Livre">Livre</option>   
-<option value="10">10 anos</option>   
-<option value="12">12 anos</option>   
-<option value="14">14 anos</option>   
-<option value="16">16 anos</option>   
-<option value="18">18 anos</option>                    
-</select> */
+closeMessage.addEventListener("click", function () {
+  message.style.display = "none";
+});
 
+setTimeout(() => {
+  message.style.display = "none";
+}, 5000);
 
-
-
-// closeMessage.addEventListener("click", function () {
-//   message.style.display = "none";
-// });
-
-// setTimeout(() => {
-//   message.style.display = "none";
-// }, 5000);
-
-
-
-// setTimeout(() => {
-//   message.style.display = "none";
-// }, 5000);
+setTimeout(() => {
+  message.style.display = "none";
+}, 5000);
