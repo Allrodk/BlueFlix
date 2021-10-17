85ef733a72ac305cbeaced8afe82a2112856977d
Adriano Lamas <lamas.free@gmail.com>
2021-10-17 00:11:44 -0300

    Up

--- views/public/js/script.js
+++ views/public/js/script.js
@@ -2,24 +2,10 @@ const closeMessage = document.querySelector("#close");
 const message = document.querySelector("#message");
 
 
-// Select via DOM
-const languagesSelect = document.getElementById("languages-select");
-const languagesList = ["Ruby", "JavaScript", "Python", "GoLang"];
+// closeMessage.addEventListener("click", function () {
+//   message.style.display = "none";
+// });
 
-languagesList.forEach((language) => {
-  option = new Option(language, language.toLowerCase());
-  languagesSelect.options[languagesSelect.options.length] = option;
-});
-
-
-
-closeMessage.addEventListener("click", function () {
-  message.style.display = "none";
-});
-
-setTimeout(() => {
-  message.style.display = "none";
-}, 5000);
-
-
-
+// setTimeout(() => {
+//   message.style.display = "none";
+// }, 5000);
