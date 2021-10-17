1e36ccffa6526963c120b277e875fb4496920903
Bruno Sallum <87883544+B-Sallum@users.noreply.github.com>
2021-10-17 18:10:05 -0300

    uptade Semântica

--- views/public/js/script.js
+++ views/public/js/script.js
@@ -1,26 +1,22 @@
 const closeMessage = document.querySelector("#close");
 const message = document.querySelector("#message");
 
-const classe = document.getElementById("inputClassificacao").value;
-const s = document.getElementById("classificacao");
+const atual = document.getElementById("classificacaoAtual").value;
+const lista = document.getElementById("classificacao");
 const options = ["Livre", "10", "12", "14", "16", "18"];
 
 options.forEach((elemento, chave) => {
-  if (elemento !== classe) {
-    s.options[s.options.length] = new Option(elemento, elemento, false, false);
+  if (elemento !== atual) {
+    lista.options[lista.options.length] = new Option(elemento, elemento, false, false);
   }
   else {
-    s.options[s.options.length] = new Option(elemento, elemento, false, true);
+    lista.options[lista.options.length] = new Option(elemento, elemento, false, true);
   }
 });
 
 closeMessage.addEventListener("click", function () {
   message.style.display = "none";
 });
-
-setTimeout(() => {
-  message.style.display = "none";
-}, 5000);
 
 setTimeout(() => {
   message.style.display = "none";
