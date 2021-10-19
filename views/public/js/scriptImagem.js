be3056080818eaf725e16898d0e6d99ef873417f
Adriano Lamas <lamas.free@gmail.com>
2021-10-19 03:58:59 -0300

    Up Imgbg

--- views/public/js/scriptImagem.js
+++ views/public/js/scriptImagem.js
@@ -2,3 +2,7 @@ const background = document.querySelector(".imagembg");
 const imagemAtual = document.getElementById("imagemAtual").value;
 
 background.style.setProperty("background-image", `url('${imagemAtual}')`);
+
+window.addEventListener("scroll", () => {
+  background.style.backgroundPosition = +window.pageYOffset + "px";
+});
